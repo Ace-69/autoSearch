b7ad1d3bc08fa380a0aaf384f89ae6b3ff493a1a
Ace-69 <francescosomma3@gmail.com>
2024-09-10 12:19:02 +0200

    Args fixed and file name change

--- main.py
+++ main.py
@@ -2,18 +2,25 @@ import pyautogui as pag
 import random
 import sys
 from time import sleep
-filePath = "valid-wordle-words.txt"
+filePath = "input.txt"
 words = []
-if (sys.argv[1]):
-    times = int(sys.argv[1])
+if (len(sys.argv)>1):
+    try:
+        times = int(sys.argv[1])
+    except ValueError:
+        print("Error: input is not an int number")
+        sys.exit()
+elif (len(sys.argv)>2):
+    print("You must pass only 1 argument")
+    sys.exit()
 else:
-    times = 36
+    times = 1
     
 with open(filePath, "r") as file:
     for count, line in enumerate(file):
         words.insert(count, line)
 
-    print(f"{len(words)} total words from wordle")
+    print(f"{len(words)} total words from input")
 
 for i in range(0, times):
     rand = random.randint(0, (len(words)-1))
